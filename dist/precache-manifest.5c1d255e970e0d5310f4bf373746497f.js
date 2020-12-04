a62d243a25ec508c278ecfe928f160b4c9aed50d
Sebastian Aretz <sebastian.aretz@santander.de>
2020-12-04 23:19:05 +0100

    chore: update dist

---
+++ dist/precache-manifest.5c1d255e970e0d5310f4bf373746497f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3f7557954ae5101f33",
+    "url": "/PWA_Sandbox/dist/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/PWA_Sandbox/dist/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ddcbdc8b1c5be8909becfa9d8b8c513e",
+    "url": "/PWA_Sandbox/dist/index.html"
+  },
+  {
+    "revision": "6a3f7557954ae5101f33",
+    "url": "/PWA_Sandbox/dist/js/app.fb3bb961.js"
+  },
+  {
+    "revision": "e28954552014552c977f",
+    "url": "/PWA_Sandbox/dist/js/chunk-vendors.1154cf97.js"
+  },
+  {
+    "revision": "af324aa900ee81856c3d9a610e6a0744",
+    "url": "/PWA_Sandbox/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/PWA_Sandbox/dist/robots.txt"
+  }
+]);
